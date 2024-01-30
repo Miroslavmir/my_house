241a028b2dd262f13562e51103d20ccb9c1e05f2
Admin <user.@mail.com>
2024-01-30 20:36:47 +0300

    The main app is ready pages index and about

--- main/urls.py
+++ main/urls.py
@@ -14,14 +14,19 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path, include
+
+from django.urls import path
 
 
+app_name = 'main'
 
+from main import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('main.urls', namespace='main'))
+
+
+    path('', views.index, name='index'),
+    path('about/', views.about, name='about'),
+
 
 ]
--- my_house/settings.py
+++ my_house/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-*9#3*58zc8b$vq7+oqvey$7$(zq8*b+bgm8@wac(_erp-7!p#0
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -118,6 +118,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'static'
+]
+
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
