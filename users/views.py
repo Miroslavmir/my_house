a3052f54f3578c14c2e36706f90c53fbdf9ec02d
Admin <user.@mail.com>
2024-02-04 13:57:32 +0300

    added notification are ready

--- users/views.py
+++ users/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render, redirect
-from users.forms import UserLoginForm, UserRegistrationForm
+from django.contrib.auth.decorators import login_required
 
-from django.contrib import auth
+from django.shortcuts import render, redirect
+from users.forms import UserLoginForm, UserRegistrationForm, ProfileForm
+
+from django.contrib import auth, messages
 from django.http import HttpResponseRedirect
 
 from django.urls import reverse
@@ -17,6 +19,7 @@ def login(request):
             user = auth.authenticate(username=username, password=password)
             if user:
                 auth.login(request, user)
+                messages.success(request, f"{username}, Вы успешно подключились на страницу")
                 return HttpResponseRedirect(reverse('main:index'))
     else:
         form = UserLoginForm()
@@ -35,9 +38,11 @@ def registration(request):
             form.save()
             user = form.instance
             auth.login(request, user)
+            messages.success(request, f"{user.username}, Вы успешно подключились на страницу и зашли в акаунт")
             return HttpResponseRedirect(reverse('main:index'))
     else:
         form = UserRegistrationForm()
+
     context = {
         'title': 'smart_house - Регистрация',
         'form': form
@@ -45,15 +50,28 @@ def registration(request):
 
     return render(request, 'users/registration.html', context)
 
-
+@login_required
 def profile(request):
+
+    if request.method == 'POST':
+        form = ProfileForm(data=request.POST, instance=request.user, files=request.FILES)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Обновление успешное")
+            return HttpResponseRedirect(reverse('user:profile'))
+    else:
+        form = ProfileForm(instance=request.user)
+
     context = {
-        'title': 'smart_house - Кабинет'
+        'title': 'smart_house - Кабинет',
+        'form': form
     }
 
     return render(request, 'users/profile.html', context)
 
+@login_required
 
 def logout(request):
+    messages.success(request, f"{request.user.username}, Вы вышли из аккаунта")
     auth.logout(request)
     return redirect(reverse('main:index'))
